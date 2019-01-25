277c18759836eeafd77237162517c574d5ae0511
f8B23Lef <mal7rish@yandex.ru>
2019-01-25 21:59:09 +0300

    fix: fix bug with babel preset

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 const path = require('path');
 
-	const conf = {
-		entry: './src/app.js',
-		output: {
-			path: path.resolve(__dirname, './dist'),
-			filename: 'main.js'
-		},
-		module: {
-		    rules: [
-          // {
-          //     test: /\.js$/,
-          //     loader: 'babel-loader',
-          //     exclude: '/node_modules/'
-          // },
-          {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-          },
-		    ]
-    },
-    mode: 'development',
-    devServer: {
-      watchOptions: {
-        poll: true
+const conf = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
       },
-      contentBase: path.join(__dirname, 'dist'),
-      compress: true,
-      port: 9000
-    }
-	};
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: 'development',
+  devServer: {
+    watchOptions: {
+      poll: true,
+    },
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
 
-	module.exports = conf;
+module.exports = conf;
