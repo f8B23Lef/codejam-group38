b16de4c8a3108005d51d1a694ca79bf0c5a255fb
Vitalik Tyrlya <39513998+vitalur@users.noreply.github.com>
2019-01-27 17:35:14 +0300

    fix: json file and writer page

--- dist/author.js
+++ dist/author.js
@@ -7,12 +7,18 @@ window.onload = () => {
 }
 
 function draw(data, i) {
-	let info = `<img src="${data[i].image}" alt="image of the author" class="col-12 col-sm-8 col-md-5 col-lg-4 image-of-the-author rounded float-left">
+	let info = `<img src="${data[i].image}" alt="image of the author" class="col-12 col-sm-8 col-md-5 col-lg-4 image-of-the-author">
 				<h2 class="information__name">${data[i].name}</h2>
 				<p class="information__biography">${data[i].biography}</p>`;
 	$('.information').append(info);
 
-	$('.books').append(data[i].books);
+	let booksArr = data[i].books;
+	let booksEl = `<ul>`;
+	for (let k = 0, len = booksArr.length; k < len; k++) {
+		booksEl += `<li>${booksArr[k]}</li>` 
+	}
+	booksEl += `</ul>`
+	$('.books').append(booksEl);
 
 	let galleryPictures = ``;
 	for (let j = 0, len = data[i].gallery.length; j < len; j++) {
